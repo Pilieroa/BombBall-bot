6b6ce750b8a0ab14e98abff1e3b948e1a1a8ddb0
Andrew Piliero <andrewpiliero@Andrews-MBP-2.attlocal.net>
2023-11-06 13:02:40 -0800

    nope just need the + and also can't have unescaped chars in the file name

--- logger.py
+++ logger.py
@@ -6,12 +6,9 @@ def log(message):
     global _current_log_file
     
     if _current_log_file == "":
-      _current_log_file = datetime.now().strftime("%m/%d/%Y, %H:%M") + ".txt"
-      with open(_current_log_file, "w") as log_file:
-        log_file.write(message + "\n\n\n")
-      return
-    
-    with open(_current_log_file, "a") as log_file:
+      _current_log_file = datetime.now().strftime("%m-%d-%Y %H-%M") + ".txt"
+   
+    with open(_current_log_file, "a+") as log_file:
         log_file.write(message + "\n\n\n")
     
 def set_log_file(name):
