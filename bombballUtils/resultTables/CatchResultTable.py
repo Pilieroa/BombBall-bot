a538c3d00635bdcb0bf11ed7629ed44c2e0711ae
Andrew Piliero <afpiliero@gmail.com>
2023-11-06 14:38:09 -0800

    - fix catch result table

--- bombballUtils/resultTables/CatchResultTable.py
+++ bombballUtils/resultTables/CatchResultTable.py
@@ -3,8 +3,8 @@ from bombballUtils.resultTables import constants
 
 class CatchResultTable(BaseResultTable):
   RESULT_ROWS = [
-      ResultTableRow(None, 6, constants.CAUGHT),
-      ResultTableRow(7, None, constants.FAILED_CATCH),
+      ResultTableRow(None, 6, constants.FAILED_CATCH),
+      ResultTableRow(7, None, constants.CAUGHT),
   ]
 
 CATCH_RESULT_TABLE = CatchResultTable()
