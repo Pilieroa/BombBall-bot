cf19dc96efdc501a223f6087f05861e7d9a8be87
Pilieroa <Pilieroa@users.noreply.github.com>
2023-11-06 20:55:50 +0000

    write permission needed to create a new file... duh

--- logger.py
+++ logger.py
@@ -6,7 +6,10 @@ def log(message):
     global _current_log_file
     
     if _current_log_file == "":
-        _current_log_file = datetime.now().strftime("%m/%d/%Y, %H:%M") + ".txt"
+      _current_log_file = datetime.now().strftime("%m/%d/%Y, %H:%M") + ".txt"
+      with open(_current_log_file, "w") as log_file:
+        log_file.write(message + "\n\n\n")
+      return
     
     with open(_current_log_file, "a") as log_file:
         log_file.write(message + "\n\n\n")
